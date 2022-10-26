296555b08d1061060d2e074899db0c5e55e29621
kevin martinez <ceronk003@gmail.com>
2022-10-25 18:43:15 -0600

    agregando cambios

--- Assets/Codigos/LogicaPersonaje1.cs
+++ Assets/Codigos/LogicaPersonaje1.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class LogicaPersonaje1 : MonoBehaviour
 {
-    public float velocidadMovimiento = 5.0f;
-    public float velocidadRotacion = 200.0f;
-    private Animator anim;
-    public float x, y;
+    public float velocidadMovimiento = 5.0f;//indicar que tan rapido se movera el personaje
+    public float velocidadRotacion = 200.0f;//velocidad para rotar el personaje
+    private Animator anim; //para obtener el componente de animator del personaje
+    public float x, y;//para saber si el personaje se esta moviendo o esta quieto
     // Start is called before the first frame update
     void Start()
     {
-        anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();//componente animator
     }
 
     // Update is called once per frame
@@ -20,7 +20,7 @@ public class LogicaPersonaje1 : MonoBehaviour
        x = Input.GetAxis("Horizontal");
        y = Input.GetAxis("Vertical");
 
-       transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0 );
+       transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0 ); //para la rotacion
        transform.Translate(0, 0 , y * Time.deltaTime * velocidadMovimiento );
 
        anim.SetFloat("VelX", x);
